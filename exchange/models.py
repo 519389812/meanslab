538fec83d2ea1fab94613bb7335a015253d9921b
519389812 <519389812@qq.com>
2022-05-22 22:40:28 +0800

    update

--- exchange/models.py
+++ exchange/models.py
@@ -8,7 +8,7 @@ class CardHolder(models.Model):
     to = models.CharField(max_length=150, blank=True, verbose_name="称呼")
     content = models.TextField(max_length=300, verbose_name="内容")
     signature = models.CharField(max_length=150, verbose_name="署名")
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="CardHolder_user", verbose_name="用户")
+    user = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.CASCADE, related_name="CardHolder_user", verbose_name="用户")
 
     class Meta:
         verbose_name = '卡包'
@@ -21,7 +21,7 @@ class CardHolder(models.Model):
 class ExchangeCard(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150, verbose_name='卡名称')
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="ExchangeCard_user", verbose_name="用户")
+    user = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.CASCADE, related_name="ExchangeCard_user", verbose_name="用户")
     used_times = models.IntegerField(default=0, verbose_name="已使用次数")
     times = models.IntegerField(verbose_name="可使用次数")
     term = models.TextField(max_length=300, blank=True, verbose_name="使用条款")
